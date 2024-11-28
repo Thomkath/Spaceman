6c2a4b3fc9e0b84e93594e2a69f79ba020e9ddc1
Thomkath <thomkath10@gmail.com>
2024-11-28 20:09:13 -0300

    Add constants and state variables for improved structure

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 /*----- constants -----*/
 
+const WORD_LIST = [
+    { word: "ASTEROID", hint: "A rocky fragment floating around space" },
+    { word: "GALAXY", hint: "A system of stars and planets" },
+    { word: "NEBULA", hint: "A giant cloud of dust and gas in space" },
+    { word: "TELESCOPE", hint: "An instrument used to view distant objects in space" },
+    { word: "SATELLITE", hint: "An object in orbit" },
+    { word: "CONSTELLATION", hint: "A group of stars that form a pattern in the sky" },
+    { word: "METEOR", hint: "A space rock that enters Earth's atmosphere" },
+    { word: "GRAVITY", hint: "A force that pulls two objects toward each other" },
+    { word: "INTERSTELLAR", hint: "Situated or occurring between the stars" },
+    { word: "PLANET", hint: "A celestial body orbiting around a star" }
+  ];
 /*----- state variables -----*/
+let triesRemaining, guessedLetters, currentWord, word, secretHint, gameStatus, beamOpacity, spacemanOpacity;
 
 /*----- cached elements  -----*/
 
 /*----- event listeners -----*/
 
 /*----- functions -----*/
+
